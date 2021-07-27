df0a60acea35b90e858cd287e8f2778c5576e38d
yahya <crazy20046@gmail.com>
2021-07-27 11:04:57 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/webpack-demo/',
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          'handlebars-loader', // handlebars loader expects raw resource string
-          'extract-loader',
-          'css-loader',
-        ],
-        test: /\.html$/i,
-        loader: 'html-loader',
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
