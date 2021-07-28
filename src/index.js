925cdb7675125f8a70da6bd2f643e2e76453140b
yahya <crazy20046@gmail.com>
2021-07-28 08:54:13 +0200

    Add Required Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
-const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xIniA5Uar0iFa7cLE5KD/scores/';
+const apiURL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/8hJ4QD4zPJVXkNmwSOKJ/scores/';
 
 const userInput = document.getElementById('user-input');
 const scoreInput = document.getElementById('score-input');
@@ -12,11 +13,13 @@ const listElement = (user, number) => {
   const li = document.createElement('li');
   li.innerText = `${user} : ${number}`;
   scoreList.appendChild(li);
-};
 
+  userInput.innerText = '';
+  scoreInput.innerText = '';
+};
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  fetch(apiURL, {
+  await fetch(apiURL, {
     method: 'POST',
     body: JSON.stringify({
       user: userInput.value,
@@ -25,16 +28,18 @@ form.addEventListener('submit', async (e) => {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).then((response) => response.json());
-});
-
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((json) => {
-    json.result.forEach((element) => {
-      listElement(element.user, element.score);
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  window.location.reload();
+}),
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((json) => {
+      json.result.forEach((element) => {
+        listElement(element.user, element.score);
+      });
     });
-  });
 
 fetch(apiURL).then((response) => response.json());
 
